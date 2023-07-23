c698c62c055a5327f59af36c3f60d1c6df371ec0
kd5200 <kareemdavis18@yahoo.com>
2023-07-23 11:46:39 -0400

    TBC

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,26 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/logo.svg';
+import { pageLinks } from '../data.js';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
+            {pageLinks.map((link)=>{
+                return (
+                    <li key={link.id}>
+            <a href={link.href} className="nav-link"> {link.text} </a>
           </li>
-
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
+                )
+            })}
         </ul>
 
         <ul className="nav-icons">
